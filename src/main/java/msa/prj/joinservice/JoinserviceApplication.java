ddc361b29bafc5c9966b9c4860547489bd511f4f
ysl2884 <ysl2884@gmail.com>
2024-10-30 13:54:49 +0900

    joinservice

---
+++ src/main/java/msa/prj/joinservice/JoinserviceApplication.java
@@ -0,0 +1,13 @@
+package msa.prj.joinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msa/prj/joinservice/JoinserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.prj.joinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
